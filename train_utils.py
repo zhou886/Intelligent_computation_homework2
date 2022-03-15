b8caedb8b53f5ac19205c3c83d1fb85276ffde62
zhou886 <1272686158@qq.com>
2022-03-15 09:36:13 +0800

    add count graph

--- train_utils.py
+++ train_utils.py
@@ -16,24 +16,12 @@ class SGD:
     def __init__(self, lr:float = 0.01) -> None:
         self.lr = lr
 
-    def back_propagation(self, result:np.ndarray, *para:np.ndarray) -> None:
-        self.w1 = para[0]
-        self.b1 = para[1]
-        self.w2 = para[2]
-        self.b2 = para[3]
-        
-        self.b2_deri = 
-
-
     def step(self) -> tuple:
         self.w1 += self.lr * self.w1_deri
 
 class Adam:
     def __init__(self, lr:float = 0.01) -> None:
         self.lr = lr
-
-    def back_propagation(self, result:np.ndarray, para:np.ndarray) -> None:
-        pass
 
     def step(self) -> None:
         pass
